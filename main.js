1c41346cb0e1975c6bdde4f333fdac0d8c100e37
BreakVoid <breakvoid@hotmail.com>
2015-07-21 14:09:25 +0800

    to simplify the program move all the content of syntax.js to main.js and delete syntax.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var syntax = require("./syntax.js");
+var crunch = require("number-crunch");
 var util = require("./util.js");
 
-var globalScope = syntax.identifiers;
+var memPool = {};
+var identifier = {};
 
 function ProcExec(str, curScope) {
 	// return a object {"type" : ...., "content" : ....}
@@ -12,7 +13,7 @@ function ProcExec(str, curScope) {
 			if (procParas[i].type == "procedure") {
 				paraList.push(ProcExec(procParas[i].content, curScope));
 			} else if (procParas[i].type == "identifier") {
-				paraList.push(syntax.memPool[curScope[procParas[i].content].uuid]);
+				paraList.push(memPool[curScope[procParas[i].content].uuid]);
 			} else {
 				paraList.push(procParas[i]);
 			}
@@ -21,6 +22,231 @@ function ProcExec(str, curScope) {
 	} else {
 	}
 }
+
+identifiers = {
+	"define" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+			var result = {};
+			if (paras[0].type == 'procedure') {
+				result.type = "function";
+				result.scope = curScope;
+				result.body = paras.slice(1, paras.length);
+				var functionForm = util.GetElements(paras[0]);
+				result.name = functionForm[0].content;
+				result.paraList = functionForm.slice(1, funtionForm.length);
+			} else {
+				result.type = util.GetType(paras[1].content);
+				result.content = paras[1].content;
+				result.name = paras[0].content;
+			}
+			return result;
+		}
+	},
+	"if" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	"cond" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+
+		}
+	},
+	"cons" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+
+		}
+	},
+	"car" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	"cdr" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	"show" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+			console.log(paras[0].content);
+		}
+	},
+	"+" : {
+		//TO DO
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+			var result = {};
+			result.type = "number-integer";
+			result.content = "0";
+			for (var i = 0; i < paras.length; ++i) {
+				if (result.type == "number-integer") {
+					if (paras[i].type == "number-integer") {
+						var tmp = crunch.parse(paras[i].content);
+						result.content = crunch.stringify(crunch.add(crunch.parse(result.content), tmp));
+					} else {
+						result.type = "number-float";
+						result.content = parseFloat(result.content);
+						var tmp = parseFloat(paras[i].content);
+						result.content += tmp;
+					}
+				} else {
+					var tmp = parseFloat(paras[i].content);
+					result.content += tmp;
+				}
+			}
+			return result;
+		}
+	},
+	"-" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+			var result = {};
+			result.type = "number-integer";
+			result.content = "0";
+			if (result.type == "number-integer") {
+				if (paras[0].type == "number-integer") {
+					result.content = paras[0].content;
+				} else {
+					result.type = "number-float";
+					result.content = parseFloat(result.content);
+					var tmp = parseFloat(paras[0].content);
+					result.content = tmp;
+				}
+			} else {
+				var tmp = parseFloat(paras[0].content);
+				result.content = tmp;
+			}
+			for (var i = 1; i < paras.length; ++i) {
+				if (result.type == "number-integer") {
+					if (paras[i].type == "number-integer") {
+						var tmp = crunch.parse(paras[i].content);
+						result.content = crunch.stringify(crunch.sub(crunch.parse(result.content), tmp));
+					} else {
+						result.type = "number-float";
+						result.content = parseFloat(result.content);
+						var tmp = parseFloat(paras[i].content);
+						result.content -= tmp;
+					}
+				} else {
+					var tmp = parseFloat(paras[i].content);
+					result.content -= tmp;
+				}
+			}
+			return result;
+		}
+	},
+	"*" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+			var result = {};
+			result.type = "number-integer";
+			result.content = "1";
+			for (var i = 0; i < paras.length; ++i) {
+				if (result.type == "number-integer") {
+					if (paras[i].type == "number-integer") {
+						var tmp = crunch.parse(paras[i].content);
+						result.content = crunch.stringify(crunch.mul(crunch.parse(result.content), tmp));
+					} else {
+						result.type = "number-float";
+						result.content = parseFloat(result.content);
+						var tmp = parseFloat(paras[i].content);
+						result.content *= tmp;
+					}
+				} else {
+					var tmp = parseFloat(paras[i].content);
+					result.content *= tmp;
+				}
+			}
+			return result;
+		}
+	},
+	"/" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+			var result = {};
+			result.type = "number-integer";
+			result.content = "0";
+			if (result.type == "number-integer") {
+				if (paras[0].type == "number-integer") {
+					result.content = paras[0].content;
+				} else {
+					result.type = "number-float";
+					result.content = parseFloat(result.content);
+					var tmp = parseFloat(paras[0].content);
+					result.content = tmp;
+				}
+			} else {
+				var tmp = parseFloat(paras[0].content);
+				result.content = tmp;
+			}
+			for (var i = 1; i < paras.length; ++i) {
+				if (result.type == "number-integer") {
+					if (paras[i].type == "number-integer") {
+						var tmp = crunch.parse(paras[i].content);
+						result.content = crunch.stringify(crunch.div(crunch.parse(result.content), tmp));
+					} else {
+						result.type = "number-float";
+						result.content = parseFloat(result.content);
+						var tmp = parseFloat(paras[i].content);
+						result.content /= tmp;
+					}
+				} else {
+					var tmp = parseFloat(paras[i].content);
+					result.content /= tmp;
+				}
+			}
+			return result;
+		}
+	},
+	">" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	"<" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	"=" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	//should support more than two expression
+	"and" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	"or" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	"not" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	"eq?" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	},
+	"equal?" : {
+		"type" : "syntax",
+		"exec" : function(paras, curScope) {
+		}
+	}
+};
+
+var globalScope = identifiers;
 
 var fs = require("fs");
 
@@ -42,7 +268,7 @@ for (var i = 0; i < sents.length; ++i) {
 				value = ProcExec(procParas[2].content, globalScope);
 				var defRes = globalScope["define"]["exec"]([procParas[1], value], globalScope);
 				var uuid = util.genUUID();
-				syntax.memPool[uuid] = defRes;
+				memPool[uuid] = defRes;
 				globalScope[defRes.name] = {
 					"uuid" : uuid,
 					"type" : "identifier"
@@ -51,7 +277,7 @@ for (var i = 0; i < sents.length; ++i) {
 			} else {
 				var defRes = globalScope["define"]["exec"](procParas.slice(1, procParas.length), globalScope);
 				var uuid = util.genUUID();
-				syntax.memPool[uuid] = defRes;
+				memPool[uuid] = defRes;
 				globalScope[defRes.name] = {
 					"uuid" : uuid,
 					"type" : "identifier"
